06f23d978506831ee38e78f7d54fd47a2de1d309
Raman <ramansah2004@gmail.com>
2026-06-26 17:39:31 +0530

    added favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -78,7 +78,9 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
     ],
-    links: [{ rel: "stylesheet", href: appCss }],
+    links: [{ rel: "stylesheet", href: appCss },
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],  // add this
+    
   }),
   shellComponent: RootShell,
   component: RootComponent,
